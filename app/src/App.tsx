2bc70cd67946692de2eecd8dd66fac1738940881
Nathan <ncmoseley@gmail.com>
2024-10-25 13:37:40 +0200

    Add start stop buttons

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    chrome.runtime.sendMessage(
+      { type: "greeting", message: "Hello from Vite!" },
+      (response) => {
+        console.log("Response from background:", response);
+      }
+    );
+  }, []);
+
+  const startRecording = () => {
+    chrome.runtime.sendMessage({ type: "start-recording" });
+  };
+
+  const stopRecording = () => {
+    chrome.runtime.sendMessage({ type: "stop-recording" });
+  };
 
   return (
     <>
@@ -12,24 +28,16 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        {/* <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        </button> */}
+        <button onClick={startRecording}>Start Recording</button>
+        <button onClick={stopRecording}>Stop Recording</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
